1618370139c256c888f1c745c72643964aef9d58
Ralph Joshua Batula <rjbatula@yahoo.com>
2020-02-28 15:20:27 +0800

    Updated Booking Date with Start and End Booking

--- DexterLab/Models/Data/BookingDTO.cs
+++ DexterLab/Models/Data/BookingDTO.cs
@@ -17,6 +17,7 @@ namespace DexterLab.Models.Data
         public string DeviceType { get; set; }
         public int DeviceSpace { get; set; }
         public DateTime BookingDate { get; set; }
+        public DateTime BookingEndDate { get; set; }
         public int PanelStart { get; set; }
         public int PanelEnd { get; set; }
         public string BookingPurpose { get; set; }
